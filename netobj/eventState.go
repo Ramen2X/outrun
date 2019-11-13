37f1547e82fd6785665e3db4c7f848ecb5177b77
FairPlay137 <danielws424@gmail.com>
2019-11-12 19:25:49 -0800

    Add a few missing files

---
+++ netobj/eventState.go
@@ -0,0 +1,22 @@
+package netobj
+
+type EventState struct {
+    Param           int64 `json:"param"`    
+    RewardID        int64 `json:"rewardId"`
+    PreviousEventID int64
+}
+
+func DefaultEventState() EventState {
+	param := int64(0)
+	rewardId := int64(0) // ???
+	previousEventId := int64(0) // no previous event
+	return NewEventState(param, rewardId, previousEventId)
+}
+
+func NewEventState(param, rewardId, previousEventId int64) EventState {
+    return EventState{
+        param,
+        rewardId,
+	previousEventId,
+    }
+}
