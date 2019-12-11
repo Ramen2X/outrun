d43d50abb7592cd462b2fa271c86751c67d4b99c
FairPlay137 <danielws424@gmail.com>
2019-12-11 01:51:59 -0800

    Add data structures for `/Message/getMessage` endpoint

--- responses/message.go
+++ responses/message.go
@@ -1,6 +1,7 @@
 package responses
 
 import (
+	"github.com/fluofoxxo/outrun/netobj"
 	"github.com/fluofoxxo/outrun/obj"
 	"github.com/fluofoxxo/outrun/responses/responseobjs"
 )
@@ -33,4 +34,14 @@ func DefaultMessageList(base responseobjs.BaseInfo) MessageListResponse {
 			obj.DefaultOperatorMessage(),
 		},
 	)
+}
+
+type GetMessageResponse struct {
+	BaseResponse
+	PlayerState                 netobj.PlayerState `json:"playerState"`
+	CharacterState              []netobj.Character `json:"characterState"`
+	ChaoState                   []netobj.Chao      `json:"chaoState"`
+	PresentList                 []obj.Present      `json:"presentList"`                // obtained gifts?
+	RemainingMessageIDs         []int64            `json:"notRecvMessageList"`         // IDs of messages not yet received?
+	RemainingOperatorMessageIDs []int64            `json:"notRecvOperatorMessageList"` // IDs of operator messages not yet received?
 }
