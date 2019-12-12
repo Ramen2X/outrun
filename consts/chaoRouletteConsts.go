1d6ac4f371f2aff9daf4bcaeb2651bac87387c36
FairPlay137 <danielws424@gmail.com>
2019-12-11 23:14:08 -0800

    Reenabled 1.1.4 Chao table Bumped down Chao roulette price to 15 red rings

--- consts/chaoRouletteConsts.go
+++ consts/chaoRouletteConsts.go
@@ -1,10 +1,10 @@
 package consts
 
 const (
-    ChaoRouletteRedRingCost            = int64(30)
-    ChaoRouletteTicketCost             = int64(1)
-    ChaoRouletteCharacterStarIncrease  = int64(1)
-    ChaoRouletteCharacterLevelIncrease = int64(5)
-    ChaoRouletteChaoLevelIncreaseLow   = int64(1)
-    ChaoRouletteChaoLevelIncreaseHigh  = int64(5)
+	ChaoRouletteRedRingCost            = int64(15)
+	ChaoRouletteTicketCost             = int64(1)
+	ChaoRouletteCharacterStarIncrease  = int64(1)
+	ChaoRouletteCharacterLevelIncrease = int64(5)
+	ChaoRouletteChaoLevelIncreaseLow   = int64(1)
+	ChaoRouletteChaoLevelIncreaseHigh  = int64(3)
 )
