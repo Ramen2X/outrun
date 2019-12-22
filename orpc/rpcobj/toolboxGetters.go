ea6ce629ad446a5d5de1845a5a2a3b43d723f105
FairPlay137 <danielws424@gmail.com>
2019-12-21 19:58:31 -0800

    Add GetTransferID RPC command

--- orpc/rpcobj/toolboxGetters.go
+++ orpc/rpcobj/toolboxGetters.go
@@ -1,55 +1,72 @@
 package rpcobj
 
 import (
-    "strconv"
+	"strconv"
 
-    "github.com/fluofoxxo/outrun/db"
+	"github.com/fluofoxxo/outrun/db"
 )
 
 func (t *Toolbox) GetUsername(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = player.Username
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = player.Username
+	return nil
 }
 
 func (t *Toolbox) GetRouletteTickets(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = strconv.Itoa(int(player.PlayerState.NumRouletteTicket))
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.PlayerState.NumRouletteTicket))
+	return nil
 }
 
 func (t *Toolbox) GetLastLogin(uid string, reply *ToolboxReply) error {
-    player, err := db.GetPlayer(uid)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Info = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Info = strconv.Itoa(int(player.LastLogin))
-    return nil
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = strconv.Itoa(int(player.LastLogin))
+	return nil
 }
 
 func (t *Toolbox) GetPersonalEvents(args ChangeValueArgs, reply *ToolboxValueReply) error {
-    player, err := db.GetPlayer(args.UID)
-    if err != nil {
-        reply.Status = StatusOtherError
-        reply.Result = "unable to get player: " + err.Error()
-        return err
-    }
-    reply.Status = StatusOK
-    reply.Result = player.PersonalEvents
-    return nil
+	player, err := db.GetPlayer(args.UID)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Result = "unable to get player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Result = player.PersonalEvents
+	return nil
+}
+
+func (t *Toolbox) GetTransferID(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	if len(player.UserPassword) == 0 {
+		reply.Status = StatusOtherError
+		reply.Info = "Player does not have usable transfer ID"
+		return nil
+	}
+	reply.Status = StatusOK
+	reply.Info = player.MigrationPassword
+	return nil
 }
