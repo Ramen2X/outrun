aaffdb127d0935cd560639d6d4a56009de945078
FairPlay137 <danielws424@gmail.com>
2019-12-24 05:17:05 -0800

    Add ResetDailyChallenge RPC command

--- orpc/rpcobj/toolboxResetters.go
+++ orpc/rpcobj/toolboxResetters.go
@@ -1,11 +1,14 @@
 package rpcobj
 
 import (
+	"math/rand"
+
 	"github.com/fluofoxxo/outrun/consts"
 	"github.com/fluofoxxo/outrun/db"
 	"github.com/fluofoxxo/outrun/enums"
 	"github.com/fluofoxxo/outrun/netobj"
 	"github.com/fluofoxxo/outrun/obj"
+	"github.com/jinzhu/now"
 )
 
 func (t *Toolbox) ResetCampaign(uid string, reply *ToolboxReply) error {
@@ -130,6 +133,33 @@ func (t *Toolbox) ResetLoginBonusState(uid string, reply *ToolboxReply) error {
 		return err
 	}
 	player.LoginBonusState = netobj.DefaultLoginBonusState(0)
+	err = db.SavePlayer(player)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to save player: " + err.Error()
+		return err
+	}
+	reply.Status = StatusOK
+	reply.Info = "OK"
+	return nil
+}
+
+func (t *Toolbox) ResetDailyChallenge(uid string, reply *ToolboxReply) error {
+	player, err := db.GetPlayer(uid)
+	if err != nil {
+		reply.Status = StatusOtherError
+		reply.Info = "unable to get player: " + err.Error()
+		return err
+	}
+	player.PlayerState.DailyChalCatNum = int64(rand.Intn(5))
+	player.PlayerState.DailyChalSetNum = int64(0)
+	player.PlayerState.DailyChalPosNum = int64(1 + rand.Intn(2))
+	player.PlayerState.DailyMissionID = int64((player.PlayerState.DailyChalCatNum * 33) + (player.PlayerState.DailyChalSetNum * 3) + player.PlayerState.DailyChalPosNum)
+	player.PlayerState.DailyMissionEndTime = now.EndOfDay().UTC().Unix()
+	player.PlayerState.DailyChallengeValue = int64(0)
+	player.PlayerState.DailyChallengeComplete = int64(0)
+	player.PlayerState.NumDailyChallenge = int64(0)
+	player.PlayerState.NextNumDailyChallenge = int64(1)
 	err = db.SavePlayer(player)
 	if err != nil {
 		reply.Status = StatusOtherError
